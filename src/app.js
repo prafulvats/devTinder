17afb3a56e4dd7fac04905d8688b55ed197a1631
prafulvats <vatsp09@gmail.com>
2024-12-28 19:44:43 +0530

    Creating Routes and Handlers

--- src/app.js
+++ src/app.js
@@ -2,6 +2,27 @@ const express = require("express");
 
 const app = express();
 
+// This will only handle get call for /user
+app.get("/user", (req, res) => {
+    console.log(req.query)
+    res.send({"firstName": "praful", "lastName": "vats"})
+});
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+    console.log(req.params)
+    res.send({"firstName": "praful", "lastName": "vats"})
+});
+
+app.post('/user', (req, res) => {
+    res.send("Data saved successfully");
+});
+
+app.delete('/user', (req, res) => {
+    res.send("Data deleted successfully");
+});
+
+//Order of the routes matter a lot
+
 app.use("/test", (req, res) => {
     res.send("hello from server test");
 });
